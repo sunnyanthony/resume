60835b5894e6f3a0505e91033e455882b7f625fb
YCC_0x0CA <sunnyanthonycheng@gmail.com>
2020-12-07 15:36:16 +0800

    initial resume by markdown

---
+++ readme.mk
@@ -0,0 +1,99 @@
+> https://www.linkedin.com/in/yu-chung-cheng-b4457a128
+> sunnyanthonycheng@gmail.com
+
+## PROFILE
+A senior level Software Engineer with 4+ years of work experience on IOT protocols, OS Kernel, system development and DevOps. Skilled in designing, writing, and developing system infrastructures and network solution. Looking for cloud/embedded system/ Linux kernel related opportunities to apply my technology skills. Holds a Master Degree in Computer Science.
+
+## PROGRAMMING LANGUAGES
+* C, C++, Python, Verilog, Rust, Shell Script, Scala, Javascript etc.
+
+## SKILLS
+- IOT protocols
+- Security
+- Linux and Network Programming
+- Linux and RTOS Kernel Development
+- Multi-task Computing
+- Tool Chain Development
+- Git and Svn
+- DevOps (Jenkins and GitLab)
+- CUDA Programming
+- Docker
+- SDN
+
+## EXPERIENCES
+### Senior Software Engineer
+> RealTek Inc. 2020 Sep. - Present
+#### NB-IOT SDK | 2020 - Present
+* System Infrastructures
+  1. Design an efficient tiny data system that backups and restores data in sleep management and guaranteeing data completeness.
+    2. Implement a port of sleep management in RealTek's boot- loader to manage the backup-memory.
+    3. Enhance FreeRTOS to support sleep mode.
+* Tools
+    1. Implement a hard-code tool that reads yaml configuration and generates a bin file to be the base of calibration setting.
+    2. Develop a CI tool to control Jenkins with remote APIs to trigger CI procedures.
+* Network Protocols and System SDK
+    1. Enhance LwIP Network stack to support sleep mode to save power while no connection in.
+    2. Enhance the protocol between the iperf2 of Cellular Callbox and the lightweight Iperf2.    
+    3. Co-implement and applying RealTek's sleep management framework APIs.
+### Software Engineer
+> RealTek Inc. 2017 Apr. - 2020 Sep.    
+> 
+#### NB-IOT SDK | 2018 - 2020
+* System Infrastructures
+    1. Ported FreeRTOS and CMSIS_OS to the OS portable layer for a new MCU/DSP.
+    2. Co-designed a multi-core message-based communication framework for making tasks to transmit messages between different OSs.
+    3. Implemented a new feature of RealTek's boot-loader to boot multiple OSs on each CPUs.
+    4. Improved the system security by TrustZone partition.
+* Tools
+    1. Implemented a post-linker tool that combined a secure ELF (TrustZone) and a non-secure ELF.
+    2. Designed an efficient multi-image placement algorithm that reduced flash fragmented memory in RealTek's post-linker tool.
+    3. Implemented experimental MIScheduler and .td file in the LLVM to utilize more Units (not adopted).
+    4. Setup Jenkins CI with Gitlab to build every commit. 
+* Network Protocols and System SDK
+    1. Improved Wakaama open source and re-designing its DTLS, datagram TLS, and connection layer with MbedTLS.
+    2. Enhanced MbedTLS on RealTek's hardware accelerator in order to speed up the encrypt, decrypt and hash calculation.
+    3. Implemented and improved network stack and tool like Ping ( ipv4 and ipv6 ), Iperf2, Cellular Interface in LwIP, ADP layer in LTE stack, UDP and IP layer in LwIP and socket APIs.
+    4. Implemented a AT agent framework to control modem system and assign network information to RealTek's netwoek interface.
+* Demo
+    1. Modified a bootstrap dashboard website to present and query our lwm2m dynamic.
+    2. Configured Nginx with fcgi to response POST request between front-end and back-end (lwm2m server).
+    3. Used Postgres to store sample data from IOT device and access by lwm2m server.
+#### LTE UE system | 2017 - 2018
+* Linux Kernel and RTOS infrastructures
+    1. Implemented remote processor message (RPMsg) virtual device that gained a memory zero-copy between two processors.
+    2. Re-wrote and improved VirtIO console device.
+    3. Maintained VirtIO net device.
+    4. Co-worked in VirtIO, vring and rproc framework.
+    5. Co-Modified QEMU to emulate our system
+* Applications
+    1. Implemented a log daemon of modem and get log from rproc device for log tracing.
+    2. Implemented a RPmsg APIs Library for user to send messages to remote processor.
+
+### TEACHING ASSISTANT
+> National Tsing Hua University 2015 - 2016
+* Programming Top 10 Important Algorithms in Python
+* Digital Logic Design
+
+## EDUCATION HIGHLIGHTS
+National Tsing Hua University
+> Taiwan
+> Computer Science | Master Degree
+* Research in [OpenFlow accelerator in SDN](https://tinyurl.com/y6rmemfe) 
+* Courses:
+    1. Parallel programming 
+    2. Introduction to Algorithms
+    3. Virtualization and Virtual Machines
+    4. Advanced Computer Architecture
+
+## CERTIFICATES
+* Coursera
+	1. Machine Learning
+	2. Artificial Intelligence - Search & Logic 
+	3. Cryptography I
+	4. Functional Programming Principles in Scala
+
+
+
+Link:
+https://github.com/sunnyanthony
+https://medium.com/@sunnyanthonycheng https://hackmd.io/@BUfe3wJ9SPWtAsmrRE7P8A https://sunnyanthony.github.io
